628fea6a7a0c9357c988e7ccfa8efd040722d9b6
Dawid Gołąb <dawidg122@interia.pl>
2023-11-11 14:32:15 +0100

    Descriptions of the functions

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -3,33 +3,33 @@ package com.company;
 import com.company.Gra;
 
 // Server
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer; // Reprezentuje server HTTP
+import com.sun.net.httpserver.HttpHandler; // Interaface do obsługi żądań HTTP
+import com.sun.net.httpserver.HttpExchange; // Obiekt reprezentujący pojedyńcze żądanie i odpowiedz HTTP
 
 // Obsługa servera
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import java.io.IOException; // to wyjątek związany z operacjami wejścia/wyjścia
+import java.io.OutputStream; // jest standardowym mechanizmem w Java do zapisywania danych binarnych, a w przypadku obsługi serwera HTTP, jest to niezbędne do wysyłania odpowiedzi z serwera do klienta.
+import java.net.InetSocketAddress; // reprezentuje adres IP i numer portu.
 
 public class Server {
     static Gra gra;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { // throws - deklaruje wyjątek(I/O)
 
         Server.gra = new Gra();
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080),0); // localhost:8080
         server.createContext("/home",new MyHandler());
-        server.start();
+        server.start(); // Uruchomienie serwera
 
 
     }
 
 
-    static class MyHandler implements HttpHandler{
+    static class MyHandler implements HttpHandler{ // implementujemy classe HttpHandler
         @Override
         public void handle(HttpExchange exchange)throws  IOException{
-            String response = "<html>" +
+            String response = "<html>" + //Tworzenie ciągu znaków response zawierającego kod HTML strony.
                     "<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">" +
                     "<div class=\"w3-panel w3-red\"><h1>Gra</h1></div>" +
                     "<div class=\"w3-panel w3-green w3-padding-16\">" + Server.gra.toStringHtml() + "</div>" +
@@ -44,8 +44,8 @@ public class Server {
                     "</script>" +
                     "</html>";
 
-            exchange.sendResponseHeaders(200,response.length()); // zadanie zostało przetworzone prawidłowo
-            OutputStream os = exchange.getResponseBody();
+            exchange.sendResponseHeaders(200,response.length()); // Wysłanie nagłówka odpowiedzi HTTP z kodem 200 (OK) i długością treści odpowiedzi.
+            OutputStream os = exchange.getResponseBody(); // Pobranie strumienia wyjściowego z obiektu HttpExchange, zapisanie w nim bajtów treści odpowiedzi, a następnie zamknięcie strumienia.
             os.write(response.getBytes());
             os.close();
         }
