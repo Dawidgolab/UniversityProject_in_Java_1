b5995fdeeb62f078f2d9fd5f67dfbd264ccff4aa
Dawidgolab <dawidg122@interia.pl>
2023-12-14 20:32:20 +0100

    Solve disappearing page error in browser

--- src/com/company/Gra.java
+++ src/com/company/Gra.java
@@ -1,25 +1,21 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.company;
 
 import java.util.Random;
 
-
-// Konstruktor - Jest to specjalna metoda, która ma tę samą nazwę co klasa i jest używana do inicjalizacji nowych obiektów:
-// 1. Konstruktor jest obowiązkowy, a jak go nie ma tworzony jest domyślny
-// 2. Musi być publiczną metodą ,
-// 3. Nazwa jego musi być taka sama jak nazwa klasy
-// 4. Może przyjmować parametry ale nie musi
-
 public class Gra {
-
     private String nazwa;
     private int liczba_iter;
     private int ogr_do;
     private int ogr_go;
     private int szukana;
-
     private int stan = -1;
 
-    public Gra(){ //Gra_
+    public Gra() {
         this.nazwa = "Gra podstawowa.";
         this.liczba_iter = 10;
         this.ogr_do = 0;
@@ -27,113 +23,118 @@ public class Gra {
         this.szukana = this.losuj();
     }
 
-    private void init(){
+    public Gra(int _liczba_iter, int _ogr_do, int _ogr_go) {
+        this.nazwa = "Gra zaawansowana.";
+        this.liczba_iter = _liczba_iter;
+        this.ogr_do = _ogr_do;
+        this.setOgr_go(_ogr_go);
+        this.szukana = this.losuj();
+    }
+
+    private void init() {
         this.stan = -1;
         this.liczba_iter = 10;
         this.szukana = this.losuj();
     }
 
-    public String nextStep (String param){
-        if (this.stan == 4 || this.stan == 3){
+    public String nextStep(String param) {
+        if (this.stan == 4 || this.stan == 3) {
             this.init();
         }
-        this.liczba_iter -= 1;
-        if (this.liczba_iter <= 0){
-            this.stan = 4;
-        }else {
 
-            if (param != "") {
-                int n = Integer.parseInt(param);
-                if (n < this.szukana) this.stan = 1;
-                if (n > this.szukana) this.stan = 2;
-                if (n == this.szukana) this.stan = 3;
+        --this.liczba_iter;
+        if (this.liczba_iter <= 0) {
+            this.stan = 4;
+        } else if (param != "") {
+            int n = Integer.parseInt(param);
+            if (n < this.szukana) {
+                this.stan = 1;
+            }
+
+            if (n > this.szukana) {
+                this.stan = 2;
+            }
+
+            if (n == this.szukana) {
+                this.stan = 3;
             }
         }
-        return this.toStringHtml();
+
+        return this.toStringHTML();
     }
 
-
-    public void print(){
-        System.out.println("==================================================");
+    public void print() {
+        System.out.println("======================================");
         System.out.print(this.toString());
-        System.out.println("----------------------------------------------------\n");
+        System.out.println("--------------------------------------\n");
     }
-
-
-// Metod o tej samej nazwie może być TYLKO jEDNA
-// !!!!!!!
-    public Gra(int _liczba_iter,int _ogr_do, int _ogr_go){  //Gra_int_int_int
-        this.nazwa = "Gra Zaawansowana";
-        this.liczba_iter = _liczba_iter;
-        this.ogr_do = _ogr_do;
-        this.setOgr_go(_ogr_go);// <---- this.ogr_go = _ogr_go;
-        this.szukana = this.losuj();
-    }
-
-// ** Hermetyzacja - określanie zakresu do którego pozwalamy się dostawać do naszej klasy/gettery i settery/
-
-    // Oczywiście, gettery i settery są metodami używanymi w Javie do dostępu do pól prywatnych w klasie z zewnątrz klasy. (np przez użytkownika)
-    // getter odczytuje prywatne dane uzytwkownika
-    // setter modyfikuje dane
 
     public int getOgr_go() {
-        return ogr_go;
+        return this.ogr_go;
     }
+
     public void setOgr_go(int _ogr_go) {
-        if (_ogr_go <= this.ogr_do){
+        if (_ogr_go <= this.ogr_do) {
             this.ogr_go = this.ogr_do + 100;
-        }else{
+        } else {
             this.ogr_go = _ogr_go;
         }
+
     }
 
-
-    private int losuj(){
+    private int losuj() {
         Random rnd = new Random();
-        return rnd.nextInt(this.ogr_do,this.ogr_go);
-        //return new Random().nextInt(this.ogr_do,this.ogr_go);
-
+        return rnd.nextInt(this.ogr_do, this.ogr_go);
     }
 
-    public String toStringHtml() {
+    public String toStringHTML() {
         String s = "<div>";
-        s += "Nazwa: " + this.nazwa + "<br>";
-        s += "Liczba iteracji: " + this.liczba_iter + "<br>"; // this = wskazuje nam element klasy
-        s += "Ograniczenie dolne: " + this.ogr_do + "<br>";
-        s += "Ograniczenie gorne: " + this.ogr_go + "<br>";
-        s += "Szukana wartosc: " + this.szukana + "<br></div>";
-        s += "<div class =\"w3-panel w3-blue\">";
-        s += "Stan Gry: " + this.stan;
-        s += "</div>";
+        s = s + "Nazwa: " + this.nazwa + "<br>";
+        s = s + "Liczba iteracji: " + this.liczba_iter + "<br>";
+        s = s + "Ograniczenie dolne: " + this.ogr_do + "<br>";
+        s = s + "Ograniczenie gorne: " + this.ogr_go + "<br>";
+        s = s + "Szukana wartosc: " + this.szukana + "<br></div>";
+        s = s + "<div class=\"w3-panel w3-blue\">";
+        if (this.stan == -1) {
+            s = s + "Zaczynamy, podaj wartosc: ";
+        }
+
+        if (this.stan == 1) {
+            s = s + "Szukana wartosc jest wieksza, podaj wartosc: ";
+        }
+
+        if (this.stan == 2) {
+            s = s + "Szukana wartosc jest mniejsza, podaj watrosc: ";
+        }
+
+        if (this.stan == 3) {
+            s = s + "Wygrana!";
+        }
+
+        if (this.stan == 4) {
+            s = s + "Przegrana!";
+        }
+
+        s = s + "</div>";
         return s;
     }
 
-
-    @Override
     public String toString() {
         String s = "";
-        s += "Nazwa: " + this.nazwa + "\n";
-        s += "Liczba iteracji: " + this.liczba_iter + "\n"; // this = wskazuje nam element klasy
-        s += "Ograniczenie dolne: " + this.ogr_do + "\n";
-        s += "Ograniczenie gorne: " + this.ogr_go + "\n";
-        s += "Szukana wartosc: " + this.szukana + "\n";
-        //-----------------------------
+        s = s + "Nazwa: " + this.nazwa + "\n";
+        s = s + "Liczba iteracji: " + this.liczba_iter + "\n";
+        s = s + "Ograniczenie dolne: " + this.ogr_do + "\n";
+        s = s + "Ograniczenie gorne: " + this.ogr_go + "\n";
+        s = s + "Szukana wartosc: " + this.szukana + "\n";
         return s;
     }
 
-
-
-    // Metoda main gdzie testujemy różne funkcje z Klasy Gra
     public static void main(String[] args) {
-
         Gra testGra1 = new Gra();
         testGra1.print();
-
-        Gra testGra2 = new Gra(100,0,1000);
+        Gra testGra2 = new Gra(100, 0, 1000);
         testGra2.print();
-
-        Gra testGra3 = new Gra(100,10,0);
+        Gra testGra3 = new Gra(100, 10, 0);
         testGra3.print();
     }
 }
-
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,49 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.company;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rnd = new Random();
-
-        int liczba_iter = 5;
+        int liczba_iter = 3;
         int ogr_do = 0;
         int ogr_go = 10;
-        int szukana = rnd.nextInt(ogr_do,ogr_go);
-        int s;
-
-
+        int szukana = rnd.nextInt(ogr_do, ogr_go);
+        System.out.println(szukana);
         boolean wygrana = false;
+        System.out.println("Rozpoczecie gry.");
 
-
-        System.out.println("Rozpoczecie gry!!!");
-
-
-        for (int i = 0; i < liczba_iter; i++) {
-            System.out.println("Podaj liczbe: ");
-            s = sc.nextInt();
-            if (szukana == s){
-                System.out.println("Zgadleś!!!");
+        for(int i = 0; i < liczba_iter; ++i) {
+            System.out.print("Podaj liczbe: ");
+            int s = sc.nextInt();
+            if (szukana == s) {
+                System.out.println("Zgadles!");
                 wygrana = true;
                 break;
-            }else{
-                if (s < szukana){
-                    System.out.println("Szukana liczba jest wieksza");
-                }else{
-                    System.out.println("Szukana liczba jest mniejsza");
-                }
+            }
+
+            if (s < szukana) {
+                System.out.println("Szukana wartosc jest wieksza.");
+            } else {
+                System.out.println("Szukana wartosc jest mniejsza.");
             }
         }
-        if (wygrana == false){
-            System.out.println("Koniec dostępnych ruchow a Twoja liczba to : " + szukana);
+
+        if (!wygrana) {
+            System.out.println("Koniec dostępnych ruchow.");
+        } else {
+            System.out.println("Koniec gry.");
         }
-        System.out.println("Koniec Gry!!!");
-
-
-
-
-
-
 
     }
 }
